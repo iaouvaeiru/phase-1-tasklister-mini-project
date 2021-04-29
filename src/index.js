153eb05bafdb6e441cd9b934eba68ab9184b512c
Leon Fan <leonkind@gmail.com>
2021-04-29 17:36:17 -0500

    added priority dropdown with corresponding colors

--- src/index.js
+++ src/index.js
@@ -5,20 +5,32 @@ document.addEventListener("DOMContentLoaded", () => {
   submitButton.addEventListener('submit', (event) => {
     event.preventDefault()
     
-    console.log(event.target['new-task-description'].value)
+    console.log(event.target['priority'].value)
     addTask(event.target['new-task-description'].value)
+
+    function addTask(task){
+      let pinnedText = document.createElement('li')
+      pinnedText.innerText = task
+      list.appendChild(pinnedText)
+      const deleteBtn = document.createElement('button')
+      deleteBtn.innerText = "X"
+      pinnedText.append(deleteBtn)
+      deleteBtn.addEventListener('click', () => {
+        pinnedText.remove()
+      })
+      if (event.target['priority'].value == 'low') {
+      pinnedText.style.color = 'green'
+      } else if (event.target['priority'].value == 'medium') {
+        pinnedText.style.color = 'orange'
+      } else if (event.target['priority'].value == 'high') {
+        pinnedText.style.color = 'red'
+      }
+      
+    }
+    
+
   })
   
-  function addTask(task){
-    let pinnedText = document.createElement('li')
-    pinnedText.innerText = task
-    list.appendChild(pinnedText)
-    const deleteBtn = document.createElement('button')
-    deleteBtn.innerText = "X"
-    pinnedText.append(deleteBtn)
-    deleteBtn.addEventListener('click', () => {
-      pinnedText.remove()
-    })
-  }
+  
 
 });
